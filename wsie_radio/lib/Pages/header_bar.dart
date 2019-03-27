995bf6903ce4bd82335d38aa75909f41ca0e3188
tcronis <46980360+tcronis@users.noreply.github.com>
2019-03-27 14:31:00 -0500

    Added the page container for all other pages

---
+++ wsie_radio/lib/Pages/header_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+Widget headerBar(){
+  final SIUERed = const Color(0xFFe41c24);
+  return new AppBar(
+    title: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'WSIE Radio ',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        Image.asset(
+          'assets/WSIE_4CBlackBackground.jpg',
+          fit: BoxFit.contain,
+        ),
+      ],
+    ),
+    backgroundColor: SIUERed,
+  );
+}
---
+++ wsie_radio/lib/Pages/page_container.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:page_indicator/page_indicator.dart';
+
+import 'header_bar.dart';
+// import 'streampage.dart';
+
+
+const SIUERed = const Color(0xFFe41c24);
+class PageHolder extends StatefulWidget{
+  @override
+  State createState() => new __PageHolder();
+}
+
+class __PageHolder extends State<PageHolder>{
+  PageController controller;
+
+  @override
+  void initState(){
+    super.initState();
+    controller = PageController();
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: headerBar(),
+      body: new Container(
+        color: Colors.white,
+        child: Container(
+          child: PageIndicatorContainer(
+            pageView: PageView(
+              children: <Widget>[
+                // StreamPage(),
+                // Text('2'),
+                // Text('3'),
+                // Text('4'),
+                
+                //Insert all of your page widgets here
+
+
+              ],
+              controller: controller,
+            ),
+            align: IndicatorAlign.bottom,
+            length: 4,
+            indicatorSpace: 10.0,
+            indicatorColor: Colors.grey,
+            indicatorSelectorColor: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
