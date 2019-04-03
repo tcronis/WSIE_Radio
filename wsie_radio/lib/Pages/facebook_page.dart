eea8ffa9ee37fe8239b05278a61f7481d26e27a9
Xalageus <andreweldern@gmail.com>
2019-04-02 21:41:48 -0500

    Card interface

--- wsie_radio/lib/Pages/facebook_page.dart
+++ wsie_radio/lib/Pages/facebook_page.dart
@@ -1,23 +1,84 @@
 import 'package:flutter/material.dart';
 
-
-class FacebookFeed extends StatefulWidget{
+class FacebookFeed extends StatefulWidget {
   @override
   State createState() => new __FacebookFeed();
 }
 
-class __FacebookFeed extends State<FacebookFeed>{
-
-   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      color: Colors.white,
-      home: Scaffold(
-        body: new Container(
-          //now insert items
-        ),
-      ),
-    );
+class __FacebookFeed extends State<FacebookFeed> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: <Widget>[
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 1'),
+                )
+              ]))),
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 2'),
+                )
+              ]))),
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 3'),
+                )
+              ]))),
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 4'),
+                )
+              ]))),
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 5'),
+                )
+              ]))),
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 6'),
+                )
+              ]))),
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 100.0,
+              child: Row(children: <Widget>[
+                Expanded(
+                  child: Text('Sample post 7'),
+                )
+              ])))
+    ]);
   }
-
 }
