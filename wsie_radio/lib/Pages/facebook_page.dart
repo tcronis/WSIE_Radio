56b0ae727e69b3deb7442115b34d38e67576c016
Xalageus <andreweldern@gmail.com>
2019-04-05 07:08:24 -0500

    Updated Facebook Page design and layout

--- wsie_radio/lib/Pages/facebook_page.dart
+++ wsie_radio/lib/Pages/facebook_page.dart
@@ -9,76 +9,223 @@ class __FacebookFeed extends State<FacebookFeed> {
   @override
   Widget build(BuildContext context) {
     return ListView(children: <Widget>[
+
+      Padding(
+        padding: const EdgeInsets.fromLTRB(20, 30, 20, 10),
+        child: Text(
+          'Facebook Feed',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 24.0),
+        ),
+      ),
+
+      Padding(
+        padding: const EdgeInsets.fromLTRB(0, 15, 0, 10),
+        child: Text(
+          'Today',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0),
+        ),
+      ),
+
       Card(
           color: Colors.white,
           elevation: 10.0,
           child: Container(
-              height: 100.0,
+              height: 80.0,
               child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
                 Expanded(
-                  child: Text('Sample post 1'),
+                  child: Text('Sample post 1 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                      style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0
+                  ),),
+                )
+              ]))),
+
+      Padding(
+        padding: const EdgeInsets.fromLTRB(0, 15, 0, 10),
+        child: Text(
+          'This Week',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0),
+        ),
+      ),
+
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 80.0,
+              child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
+                Expanded(
+                  child: Text('Sample post 2 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0
+                    ),),
+                )
+              ]))),
+
+
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 80.0,
+              child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
+                Expanded(
+                  child: Text('Sample post 3 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0
+                    ),),
+                )
+              ]))),
+
+      Padding(
+        padding: const EdgeInsets.fromLTRB(0, 15, 0, 10),
+        child: Text(
+          'This Month',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 20.0),
+        ),
+      ),
+
+      Card(
+          color: Colors.white,
+          elevation: 10.0,
+          child: Container(
+              height: 80.0,
+              child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
+                Expanded(
+                  child: Text('Sample post 4 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0
+                    ),),
                 )
               ]))),
       Card(
           color: Colors.white,
           elevation: 10.0,
           child: Container(
-              height: 100.0,
+              height: 80.0,
               child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
                 Expanded(
-                  child: Text('Sample post 2'),
+                  child: Text('Sample post 5 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0
+                    ),),
                 )
               ]))),
       Card(
           color: Colors.white,
           elevation: 10.0,
           child: Container(
-              height: 100.0,
+              height: 80.0,
               child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
                 Expanded(
-                  child: Text('Sample post 3'),
+                  child: Text('Sample post 6 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0
+                    ),),
                 )
               ]))),
       Card(
           color: Colors.white,
           elevation: 10.0,
           child: Container(
-              height: 100.0,
+              height: 80.0,
               child: Row(children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: Icon(
+                    Icons.image,
+                    size: 70.0,
+                    color: Colors.grey,
+                  ),
+                ),
+
                 Expanded(
-                  child: Text('Sample post 4'),
+                  child: Text('Sample post 7 - Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16.0
+                    ),),
                 )
               ]))),
-      Card(
-          color: Colors.white,
-          elevation: 10.0,
-          child: Container(
-              height: 100.0,
-              child: Row(children: <Widget>[
-                Expanded(
-                  child: Text('Sample post 5'),
-                )
-              ]))),
-      Card(
-          color: Colors.white,
-          elevation: 10.0,
-          child: Container(
-              height: 100.0,
-              child: Row(children: <Widget>[
-                Expanded(
-                  child: Text('Sample post 6'),
-                )
-              ]))),
-      Card(
-          color: Colors.white,
-          elevation: 10.0,
-          child: Container(
-              height: 100.0,
-              child: Row(children: <Widget>[
-                Expanded(
-                  child: Text('Sample post 7'),
-                )
-              ])))
     ]);
   }
 }
