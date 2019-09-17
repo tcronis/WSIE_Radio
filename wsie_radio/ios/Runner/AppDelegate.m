2e3dcc30473e51fbf1d6bbb157f29f951bea8729
Elder <anelder@eb2029-01.cs.siue.edu>
2019-09-17 13:16:01 -0500

    Calling function on ios

--- wsie_radio/ios/Runner/AppDelegate.m
+++ wsie_radio/ios/Runner/AppDelegate.m
@@ -5,9 +5,27 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                    methodChannelWithName:@"wsie.get.radio/stream"
+                                    binaryMessenger: controller];
+    
+    __weak typeof(self) weakSelf = self;
+    [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        // Note: this method is invoked on the UI thread.
+        if([@"playStream" isEqualToString:call.method]) {
+            [weakSelf startPlaying];
+        }
+    }];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)startPlaying{
+    NSLog(@"Hello world");
 }
 
 @end
