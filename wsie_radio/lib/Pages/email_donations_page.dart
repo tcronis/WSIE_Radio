292ec7a1b3185816499a2f2896282e36f3776bdb
tcronis <46980360+tcronis@users.noreply.github.com>
2019-09-30 13:37:10 -0500

    Changed a node focus to fix the keyboard staying open on iOS

--- wsie_radio/lib/Pages/email_donations_page.dart
+++ wsie_radio/lib/Pages/email_donations_page.dart
@@ -38,231 +38,236 @@ class __EmailAndDonations extends State<EmailAndDonations>{
       home: Scaffold(
         resizeToAvoidBottomPadding: false,
         body: new Container(
-          child: new ListView.builder(
-            itemCount: 1,
-            itemBuilder: (BuildContext context, int index){
-              return Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 15.0),
-                    child: Text(
-                      'Song Requests and More Information:',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 22.0),
-                    ),
-                  ),
-                  new Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 5, 10, 20),
-                        child: Container(
-                          child: new Text("From: ",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20.0
-                            ),
-                          ),
-                          alignment: FractionalOffset(.06,.05),
-                        ),
+          child: new GestureDetector(
+            onTap: (){
+              FocusScope.of(context).requestFocus(new FocusNode());
+            },
+            child: new ListView.builder(
+              itemCount: 1,
+              itemBuilder: (BuildContext context, int index){
+                return Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 0, 20.0, 15.0),
+                      child: Text(
+                        'Song Requests and More Information:',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22.0),
                       ),
-
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-                        child: SizedBox(
-                          height: 75.0,
-                          width: (MediaQuery.of(context).size.width)*0.71,
-                          child: TextFormField(
-                            controller: nameController,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Name",
+                    ),
+                    new Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 5, 10, 20),
+                          child: Container(
+                            child: new Text("From: ",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0
+                              ),
                             ),
-                            maxLines: 1,
+                            alignment: FractionalOffset(.06,.05),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
 
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20, 5, 15, 10),
-                child: SizedBox(
-                  height: 80.0,
-                  width: (MediaQuery.of(context).size.width)*0.87,
-                  child: TextFormField(
-                    controller: messageController,
-                    //autofocus: true,
-                    onEditingComplete: deactivate,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: "Message",
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                          child: SizedBox(
+                            height: 75.0,
+                            width: (MediaQuery.of(context).size.width)*0.71,
+                            child: TextFormField(
+                              controller: nameController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Name",
+                              ),
+                              maxLines: 1,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    maxLines: 10,
+
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 5, 15, 10),
+                  child: SizedBox(
+                    height: 80.0,
+                    width: (MediaQuery.of(context).size.width)*0.87,
+                    child: TextFormField(
+                      controller: messageController,
+                      //autofocus: true,
+                      onEditingComplete: deactivate,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Message",
+                      ),
+                      maxLines: 10,
+                    ),
                   ),
                 ),
-              ),
-                  new Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 6, 10, 0),
-                        child: Container(
-                          child: new Text("Email: ",
+                    new Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 6, 10, 0),
+                          child: Container(
+                            child: new Text("Email: ",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20.0
+                              ),
+                            ),
+                            alignment: FractionalOffset(.06,.05),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 9, 0, 0),
+                          child: SizedBox(
+                            height: 60.0,
+                            width: (MediaQuery.of(context).size.width)*0.71,
+                            child: TextFormField(
+                              controller: emailController,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Email",
+                              ),
+                              maxLines: 1,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    new Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(5, 18, 0, 0),
+                          child: Container(
+                            child: new Checkbox(
+                              value: checkVal,
+                              onChanged: (bool value){
+                                setState(() {
+                                  checkVal = value;
+                                });
+                              }
+                            ),
+                            alignment: FractionalOffset(.06,.05),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Container(
+                            child: new Text("Register for WSIE Emailing List",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15.5
+                              ),
+                            ),
+                            alignment: FractionalOffset(.06,.05),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(12, 20, 0, 0),
+                          child: RaisedButton(
+                            onPressed: _sendEmailMessage,
+                            child: Text(
+                              'Send',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0
+                              ),
+                            ),
+                          ),
+                        ),
+
+                      ],
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(20.0, 12, 20.0, 0),
+                      child: Text(
+                        'Underwriters:',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 22.0),
+                      ),
+                    ),
+
+                    Divider(),
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: new SizedBox(
+                        height: 25,
+                        width:  375,
+                        child: Text(
+                          'Logo - Underwriter 1: Link to Website/Info',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: new SizedBox(
+                        height: 25,
+                        width:  375,
+                        child: Text(
+                          'Logo - Underwriter 2: Link to Website/Info',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+
+                    Divider(),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                      child: new SizedBox(
+                        height: 25,
+                        width:  375,
+                        child: Text(
+                          'Logo - Underwriter 3: Link to Website/Info',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                    Divider(),
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                      child: ButtonTheme(
+                        buttonColor: Colors.grey,
+                        minWidth: 200.0,
+                        height: 40.0,
+                        child: RaisedButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Donations',
                             style: TextStyle(
                                 color: Colors.black,
                                 fontSize: 20.0
                             ),
                           ),
-                          alignment: FractionalOffset(.06,.05),
-                        ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 9, 0, 0),
-                        child: SizedBox(
-                          height: 60.0,
-                          width: (MediaQuery.of(context).size.width)*0.71,
-                          child: TextFormField(
-                            controller: emailController,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Email",
-                            ),
-                            maxLines: 1,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  new Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(5, 18, 0, 0),
-                        child: Container(
-                          child: new Checkbox(
-                            value: checkVal,
-                            onChanged: (bool value){
-                              setState(() {
-                                checkVal = value;
-                              });
-                            }
-                          ),
-                          alignment: FractionalOffset(.06,.05),
-                        ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                        child: Container(
-                          child: new Text("Register for WSIE Emailing List",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 15.5
-                            ),
-                          ),
-                          alignment: FractionalOffset(.06,.05),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(12, 20, 0, 0),
-                        child: RaisedButton(
-                          onPressed: _sendEmailMessage,
-                          child: Text(
-                            'Send',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18.0
-                            ),
-                          ),
-                        ),
-                      ),
-
-                    ],
-                  ),
-
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20.0, 12, 20.0, 0),
-                    child: Text(
-                      'Underwriters:',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 22.0),
-                    ),
-                  ),
-
-                  Divider(),
-
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: new SizedBox(
-                      height: 25,
-                      width:  375,
-                      child: Text(
-                        'Logo - Underwriter 1: Link to Website/Info',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: new SizedBox(
-                      height: 25,
-                      width:  375,
-                      child: Text(
-                        'Logo - Underwriter 2: Link to Website/Info',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: new SizedBox(
-                      height: 25,
-                      width:  375,
-                      child: Text(
-                        'Logo - Underwriter 3: Link to Website/Info',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    child: ButtonTheme(
-                      buttonColor: Colors.grey,
-                      minWidth: 200.0,
-                      height: 40.0,
-                      child: RaisedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Donations',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0
-                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            },            
-          ),
+                  ],
+                );
+              },            
+            ),
+          )
         ),
       ),
     );
