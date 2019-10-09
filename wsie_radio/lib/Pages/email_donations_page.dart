342773598c8f0e2e711cb965fb7aa87fedc3c971
Benjamin <benjaminbrownahs@gmail.com>
2019-10-09 18:41:47 -0500

    Set formID to real WSIE form page,added conf popup

--- wsie_radio/lib/Pages/email_donations_page.dart
+++ wsie_radio/lib/Pages/email_donations_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 class EmailAndDonations extends StatefulWidget{
   @override
@@ -20,15 +22,87 @@ class __EmailAndDonations extends State<EmailAndDonations>{
     super.dispose();
   }
 
-  void _sendEmailMessage() async {
+  void _showDialog() async{
+    // flutter defined function
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // return object of type Dialog
+        return AlertDialog(
+          title: new Text("Submission Confirmation"),
+          content: new Text("Are you sure you want to send this message?"),
+          actions: <Widget>[
+            new FlatButton(
+              child: new Text("Cancel"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            new FlatButton(
+              child: new Text("Send"),
+              onPressed: () {
+                _sendEmailMessage();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<http.Response> _sendEmailMessage() async {
+
     String email = emailController.text;
     String message = messageController.text;
     String name = nameController.text;
     String checkboxVal = checkVal.toString();
 
-    print('Name: $name with Email: $email with Message: $message List?: $checkboxVal');
-    //emailController.dispose();
-    //messageController.dispose();
+    //found at the end of form url, change to change form
+    String FORMID = '90974693484979';
+
+    //found by inspecting textboxes on form page, must be changed if changing forms
+    String INPUT1 = 'q3_fullName';
+    String INPUT2 = 'q4_email';
+    String INPUT3 = 'q5_commentsquestions';
+
+    //92640823556158 - Bens test
+    //92466756779984 - WSIE CLONE
+    //90974693484979 - WSIE REAL
+
+    String url = 'https://submit.jotformpro.com/submit/$FORMID/';
+
+    //adds notice to message if mailing list checkbox is checked
+    if( checkboxVal == 'true'){
+      String check = "\n\n[Add to Mailing List]";
+      message = message + check;
+    }
+
+    Map<String, String> _body = {
+      "formID" : "$FORMID",
+      "$INPUT1" : "$name",
+      "$INPUT2" : "$email",
+      "$INPUT3" : "$message",
+      "website" : "",
+      "simple_spc" : "$FORMID-$FORMID",
+      "event_id" : ""
+    };
+
+    //Headers used in application, must use the /x-www-form-erlencoded
+    Map<String,String> _headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "application/json",
+    };
+
+    //Make POST Request
+    final response = await http.post(url, headers: _headers,  body: _body);
+
+    //printing response and clearing the text fields
+    print(response.statusCode);
+    setState(() {});
+    emailController.clear();
+    messageController.clear();
+    nameController.clear();
   }
 
     @override
@@ -168,7 +242,7 @@ class __EmailAndDonations extends State<EmailAndDonations>{
                       Padding(
                         padding: const EdgeInsets.fromLTRB(12, 20, 0, 0),
                         child: RaisedButton(
-                          onPressed: _sendEmailMessage,
+                          onPressed: ()=> _showDialog(),
                           child: Text(
                             'Send',
                             style: TextStyle(
@@ -182,83 +256,6 @@ class __EmailAndDonations extends State<EmailAndDonations>{
                     ],
                   ),
 
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(20.0, 12, 20.0, 0),
-                    child: Text(
-                      'Underwriters:',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 22.0),
-                    ),
-                  ),
-
-                  Divider(),
-
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: new SizedBox(
-                      height: 25,
-                      width:  375,
-                      child: Text(
-                        'Logo - Underwriter 1: Link to Website/Info',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: new SizedBox(
-                      height: 25,
-                      width:  375,
-                      child: Text(
-                        'Logo - Underwriter 2: Link to Website/Info',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: new SizedBox(
-                      height: 25,
-                      width:  375,
-                      child: Text(
-                        'Logo - Underwriter 3: Link to Website/Info',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    child: ButtonTheme(
-                      buttonColor: Colors.grey,
-                      minWidth: 200.0,
-                      height: 40.0,
-                      child: RaisedButton(
-                        onPressed: () {},
-                        child: Text(
-                          'Donations',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20.0
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
                 ],
               );
             },            
